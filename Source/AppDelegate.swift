6501c30385d7f169a362c439cb6220c675998078
William Irwin-Harris <w.irwinharris@gmail.com>
2016-07-26 23:39:53 +1200

    Add basic menu bar app functionality

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -5,13 +5,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(statusBarButtonClicked)
+        }
+
+        let menu = NSMenu()
+
+        menu.addItem(NSMenuItem(title: "Quit Asset Pipeline",
+                               action: #selector(quit),
+                        keyEquivalent: "q"))
+
+        statusItem.menu = menu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+
+    func statusBarButtonClicked(sender: AnyObject) {
+        print("Hello world")
+    }
+
+    func quit(sender: AnyObject) {
+        NSApp.terminate(self);
     }
 
 }
