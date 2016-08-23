b4970112874ab6b970eadf8123c9c1e756f85f7e
William Irwin-Harris <w.irwinharris@gmail.com>
2016-08-23 19:41:45 +1200

    Add a 'Create Project' dialog with basic interactivity

--- Source/Mac/Helper/ProjectsWindowController.swift
+++ Source/Mac/Helper/ProjectsWindowController.swift
@@ -5,6 +5,10 @@ class ProjectsWindowController: NSObject, NSWindowDelegate {
     @IBOutlet weak var window : NSWindow!
     @IBOutlet weak var tableView : NSTableView!
 
+    @IBOutlet weak var creationWindow : NSWindow!
+    @IBOutlet weak var projectNameField : NSTextField!
+    @IBOutlet weak var projectDirectoryField : NSTextField!
+
     func windowDidResignKey(notification: NSNotification) {
     }
 
@@ -17,6 +21,44 @@ class ProjectsWindowController: NSObject, NSWindowDelegate {
     @IBAction
     func closeWindow(sender: AnyObject) {
         window.orderOut(nil)
+    }
+
+    @IBAction
+    func createProject(sender: AnyObject) {
+        window.beginSheet(creationWindow,
+                          completionHandler: { (returnCode) -> Void in
+
+        })
+        NSApp.activateIgnoringOtherApps(true)
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    @IBAction
+    func createProjectCreateClicked(sender: AnyObject) {
+        window.endSheet(creationWindow, returnCode: NSModalResponseOK)
+    }
+
+    @IBAction
+    func createProjectCancelClicked(sender: AnyObject) {
+        window.endSheet(creationWindow, returnCode: NSModalResponseCancel)
+    }
+
+    @IBAction
+    func createProjectSelectProjectDirectory(sender: AnyObject) {
+        let panel = NSOpenPanel();
+        panel.allowsMultipleSelection = false
+        panel.canChooseDirectories = true
+        panel.canCreateDirectories = false
+        panel.canChooseFiles = false
+        panel.beginSheetModalForWindow(creationWindow,
+                                       completionHandler: { (returnCode) -> Void in
+            if returnCode == NSFileHandlingPanelOKButton {
+                if let URL = panel.URL {
+                    let str = URL.path!
+                    self.projectDirectoryField.stringValue = str
+                }
+            }
+        })
     }
 
 }
